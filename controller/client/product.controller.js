12d2cf138dd634ac3ace56c03e738a37ccb2d2e9
Nguyễn Tiến Mạnh <nguyenmanh002347@gmail.com>
2026-06-09 22:40:47 +0700

    create dashboad page

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,3 +1,17 @@
-module.exports.index = (req, res) => {
-  res.render("client/pages/products/index.pug");
+const Product = require("../../models/products.model")
+
+module.exports.index = async(req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  })
+  const newProduct = products.map(item =>{
+    item.newPrice = (item.price * (100 - item.discountPercentage)/100).toFixed(0)
+    return item;
+  })
+  console.log("product",newProduct);
+  res.render("client/pages/products/index.pug",{
+    pageTitle : "Danh  sách sản phẩm",
+    products : newProduct
+  });
 }
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const database = require("./config/database")
 require("dotenv").config();
 const route = require("./router/client/index.route");
+const routeAdmin = require("./router/admin/index.route");
+
+
+
 
 const app = express();
 const port = process.env.PORT;
+database.connect();
 
 route(app);
+routeAdmin(app);
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static('public'));
+
 
 
 
