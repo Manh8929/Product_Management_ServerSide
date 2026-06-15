b99320930b83fca743ae4e0d8c30d71bb2a25415
Nguyễn Tiến Mạnh <nguyenmanh002347@gmail.com>
2026-06-15 23:30:39 +0700

    optimation pagitation feature

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,40 +1,40 @@
 const Product = require("../../models/products.model");
-const filterStatusHelper = require("../../helpers/filterStatusHelper")
-const searchHelper = require("../../helpers/searchHelper")
+const filterStatusHelper = require("../../helpers/filterStatusHelper");
+const searchHelper = require("../../helpers/searchHelper");
+const paginationHelper = require("../../helpers/paginationHelper");
 // [GET] admin/products
 module.exports.product = async (req, res) => {
-  const filterStatus = filterStatusHelper(req.query)
+  const filterStatus = filterStatusHelper(req.query);
   let find = {
     deleted: false,
   };
   if (req.query.status) {
     find.status = req.query.status;
   }
-  const objSearch = searchHelper(req.query)
+  const objSearch = searchHelper(req.query);
   if (objSearch.regex) {
     find.title = objSearch.regex;
   }
   //Pagination
-  let objPagination = {
-    currentPage: 1,
-    limitItem: 4
-  }
-  if(req.query.page){
-    objPagination.currentPage = parseInt( req.query.page)
-  }
-  objPagination.skip = (objPagination.currentPage - 1)*objPagination.limitItem;
-  const countsProduct = await Product.countDocuments(find)
-  const totalPage = Math.ceil(countsProduct/objPagination.limitItem)
-  console.log(totalPage);
-  objPagination.totalPage = totalPage
+  const countsProduct = await Product.countDocuments(find);
+  let objPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItem: 4,
+    },
+    req.query,
+    countsProduct,
+  );
 
-  const products = await Product.find(find).limit(objPagination.limitItem).skip(objPagination.skip);
+  const products = await Product.find(find)
+    .limit(objPagination.limitItem)
+    .skip(objPagination.skip);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sach san pham",
     products: products,
     filterStatus: filterStatus,
     keyword: objSearch.keyword,
-    pagination: objPagination
+    pagination: objPagination,
   });
 };
---
+++ helpers/paginationHelper.js
@@ -0,0 +1,10 @@
+module.exports = (objPagination,query,countsProduct)=> {
+  if(query.page){
+    objPagination.currentPage = parseInt( query.page)
+  }
+  objPagination.skip = (objPagination.currentPage - 1)*objPagination.limitItem;
+  const totalPage = Math.ceil(countsProduct/objPagination.limitItem)
+  console.log(totalPage);
+  objPagination.totalPage = totalPage
+  return objPagination;
+}
